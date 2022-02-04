5ebb704d1d47d104e98f0f4b52d685d7bf39daaf
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2022-02-04 18:00:06 +0100

    Update some types

--- src/interfaces/KeyStorage.ts
+++ src/interfaces/KeyStorage.ts
@@ -3,7 +3,7 @@ import {KeyWithMaterial} from "./KeyWithMaterial";
 
 
 export interface KeyStorage {
-    saveAsymmetricKey(keyType: KeyTypes.ECDH_PRIVATE_KEY | KeyTypes.ECDH_PUBLIC_KEY | KeyTypes.RSA_PRIVATE_KEY | KeyTypes.RSA_PUBLIC_KEY, identifier:string, key:string|JsonWebKey): Promise<string>
+    saveAsymmetricKey(keyType: KeyTypes.ECDH_PRIVATE_KEY | KeyTypes.ECDH_PUBLIC_KEY | KeyTypes.RSA_PRIVATE_KEY | KeyTypes.RSA_PUBLIC_KEY, identifier:string, key:string|JsonWebKey|KeyWithMaterial): Promise<string>
     savePBKDFKey(keyType: KeyTypes.PBKDF2_KEY, identifier:string, key:JsonWebKey, salt: Uint8Array): Promise<string>
     saveAESKey(keyType: KeyTypes.AES_KEY, identifier:string, key:string, iv: Uint8Array): Promise<string>
 
