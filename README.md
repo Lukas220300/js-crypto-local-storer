92835ec9c9f03734d068b43af28aca8f27691a7f
Lukas Schönbeck <lukas.schoenbeck@gmx.de>
2022-02-04 11:37:45 +0100

    add db schema definition

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # js-crypto-local-storer
+
+### methods
+- remove all data
+
+### tables
+- pbkdf
+  - key: uuid
+  - value: pbkdf-Key as jwk (plaintext) 
+- ecdhPrivateKeys
+  - key: user identifier
+  - value: key as jwk (encrypted)
+- ecdhPublicKeys
+  - key: user identifier
+  - value: key as jwk (plaintext)
+- asymmetricPrivateKeys
+  - key: user identifier
+  - value: key as jwk (encrypted)
+- asymmetricPublicKeys
+  - key: user identifier
+  - value: key as jwk (plaintext)
+- aesKeys
+  - key: identifier uuid
+  - value: key as jwk (encrypted)
--- src/indexedDB/schemas/StorageSchema.ts
+++ src/indexedDB/schemas/StorageSchema.ts
@@ -1,5 +1,28 @@
 import {DBSchema} from "idb";
 
 export interface StorageSchema extends DBSchema {
-
+    pbkdf: {
+        key: string,
+        value: JsonWebKey,
+    },
+    ecdhPrivateKeys: {
+        key: string,
+        value: string,
+    },
+    ecdhPublicKeys: {
+        key: string,
+        value: JsonWebKey,
+    },
+    asymmetricPrivateKeys: {
+        key: string,
+        value: string,
+    },
+    asymmetricPublicKeys: {
+        key: string,
+        value: JsonWebKey,
+    },
+    aesKeys: {
+        key: string,
+        value: string,
+    },
 }
